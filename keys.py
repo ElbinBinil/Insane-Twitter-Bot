053e65fad4f7c487b64a118bd0f71e2abb6ec72e
Elbin Binil <elbinbinil@gmail.com>
2021-02-08 21:26:18 +0530

    This is intiatial commit

---
+++ keys.py
@@ -0,0 +1,6 @@
+# Twitter application consumer keys and access tokens.
+# Remember, keep this information private.
+consumer_key = environ['CONSUMER_KEY']
+consumer_secret = environ['CONSUMER_SECRET']
+access_token = environ['ACCESS_TOKEN']
+access_token_secret = environ['ACCESS_TOKEN_SECRET']
---
+++ retweet.py
@@ -0,0 +1,37 @@
+# Retweet bot for Twitter, using Python and Tweepy.
+# Search query via hashtag or keyword.
+
+
+import tweepy
+from time import sleep
+# Import in your Twitter application keys, tokens, and secrets.
+# Make sure your keys.py file lives in the same directory as this .py file.
+from keys import *
+import os 
+from os import environ
+
+
+auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+auth.set_access_token(access_token, access_token_secret)
+api = tweepy.API(auth)
+
+# Where q='#example', change #example to whatever hashtag or keyword you want to search.
+# Where items(100), change 100 to the amount of retweets you want to tweet.
+# Make sure you read Twitter's rules on automation - don't spam!
+for tweet in tweepy.Cursor(api.search, q='#100DaysOfCode').items(100):
+    try:
+        print('\nRetweet Bot found tweet by @' + tweet.user.screen_name + '. ' + 'Attempting to retweet.')
+
+        tweet.retweet()
+        print('Retweet published successfully.')
+
+        
+        sleep(10)
+
+    # Some basic error handling. Will print out why retweet failed, into your terminal.
+    except tweepy.TweepError as error:
+        print('\nError. Retweet not successful. Reason: ')
+        print(error.reason)
+
+    except StopIteration:
+        break
